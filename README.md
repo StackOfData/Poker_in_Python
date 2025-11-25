1b425d90a80ed6e4c7e10df24ec0be3130eb16b5
Lucas CHEEKHOOREE <merlin.berlan@gmail.com>
2025-11-25 15:21:25 +0100

    TODO end of turn with all the points distribution

---
+++ README.md
@@ -0,0 +1,10 @@
+# Poker in Python
+
+## Resume
+
+Hi everyone, this program is an attempt to speed run a poker game in Python. This way I can practice basics of Python
+
+## TODO Check
+* [NOK] Create points mecanism 
+* [NOK] Optimize code
+* [NOK] Add an interface
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
 
 class Player():
-    def __init__(self, wallet, inGame, isFold, isAllIn):
+    def __init__(self, wallet, inGame, inGameBet, isFold, isAllIn):
         self.wallet = 0
         self.inGame = [0,0,0]
+        self.inGameBet = 0
         self.isFold = False
         self.isAllIn = False
 
@@ -21,6 +22,7 @@ class Player():
                     print(f"ERROR NONE OF YOUR INPUT IS RIGHT")
         # Bet Case
         self.inGame[round] = int(nbBet)
+        self.inGameBet = (nbBet - self.inGame[round]) 
         self.wallet = self.wallet - (nbBet - self.inGame[round])
 
 
@@ -31,7 +33,11 @@ class Player():
         self.inGame[round] = self.wallet
         self.wallet = 0
         self.isAllIn = True
-        
+
+    def resetBet(self):
+        self.inGameBet = 0
+        for i in range(0,2):
+            self.inGame[i] = 0
 
 
 def runGame():
@@ -45,9 +51,10 @@ def runGame():
         PlayerArray.append(Player(0,[0,0,0],False,False))
 
     while(True):
+        allInBool = False
         for i in range(0,2):
             # Round
-            print(f"Round {i}")
+            print(f"######## Round {i} #########")
 
             sameBetBool = False
 
@@ -58,7 +65,7 @@ def runGame():
                     if PlayerArray[j].isFold == True or PlayerArray[j].isAllIn == True:
                         continue
                     # Player action
-                    print(f"It's player {j} turn {i}")
+                    print(f"#### It's player {j} turn {i}")
 
                     # Action menu
                     decision = input(f"What's your game? Fold, Follow, Bet (You got {PlayerArray[j].wallet}, actual bet = {bet} :)")
@@ -71,6 +78,8 @@ def runGame():
                         case "Bet":
                             nbBet = input("How much do you want to bet: ")
                             PlayerArray[j].bet(i,nbBet)
+                            if PlayerArray[j].isAllIn:
+                                allInBool = True
                             bet = nbBet
                             print(f"You bet {bet}")
 
@@ -85,7 +94,32 @@ def runGame():
                     else:
                         sameBetBool = True
 
+            # END OF ROUND
+            print(f"#### END OF TURN {i}")
+            print(f"#### Recap of each player statu")
+            for j in range (0,nbPlayers):
+                print(f"Player {j} wallet {PlayerArray[j].wallet}")
 
+        # END OF TURN
+        winner = int(input(f"####  Who is the winner 1...n"))
+        winnerJackpot = 0
+        # Compute every bet in the Jackpot
+        # if allInBool == False:
+        #     # Case 'classic'
+        #     for j in range(0,nbPlayers):
+        #         winnerJackpot = winnerJackpot + PlayerArray[j].inGameBet
+        #         PlayerArray[j].resetBet()
+        #         PlayerArray[winner].wallet =  PlayerArray[winner].wallet + winnerJackpot
+        # else:
+        #     # Case all In
+        #     for j in range(0,nbPlayers):
+        #         winnerJackpot = winnerJackpot + abs(PlayerArray[j].inGameBet - PlayerArray[winner].inGameBet)
+        #         PlayerArray[j].wallet = PlayerArray[j].wallet + 
+        #         if PlayerArray[winner].inGameBet >= PlayerArray[j].inGameBet:
+        #             winnerJackpot = winnerJackpot + PlayerArray[j].inGameBet
+        #             PlayerArray[j].inGameBet = 0
+        #         else:
+                    
 
 if __name__ == '__main__':
     runGame()
